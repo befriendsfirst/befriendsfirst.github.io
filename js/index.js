31d9dea64eb7c9d9437779cb8b874610ee651718
befriendsfirst <beefriendsfirst@gmail.com>
2016-10-18 00:59:53 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
- 	$(".landing").hide().fadeIn(4000);
+	$(".landing").hide().fadeIn(4000);
 	$("#opening").fadeIn(7000);
- 	$("#message").fadeIn(15000);
+  	$("#message").fadeIn(15000);
 });
